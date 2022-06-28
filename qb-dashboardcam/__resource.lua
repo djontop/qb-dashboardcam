9d1267dccff02f1a7867a7b75ed7776bab5376b6
DJ On Top <107875648+djontop@users.noreply.github.com>
2022-06-29 03:42:18 +0530

    Add files via upload

---
+++ qb-dashboardcam/__resource.lua
@@ -0,0 +1,18 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+author 'DJ ✔ 👑#5537'
+
+ui_page "nui/index.html"
+files {
+    "nui/index.html",
+    "nui/vue.min.js",
+    "nui/script.js",
+    "nui/style.css",
+    "nui/images/seal.png"
+}
+
+client_scripts {
+    "config.lua",
+    "client.lua"
+}
+-----https://github.com/djontop-----
---
+++ qb-dashboardcam/config.lua
@@ -0,0 +1,12 @@
+DashcamConfig = {}
+
+DashcamConfig.useMPH = false -- False will turn it to KMH
+
+DashcamConfig.RestrictVehicles = true
+DashcamConfig.RestrictionType = "class" -- custom / class
+
+DashcamConfig.AllowedVehicles = {
+    "police2",
+}
+
+-----https://github.com/djontop-----
